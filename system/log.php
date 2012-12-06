f2922a39814ae8c902be498a793fc85cd822651d
Alexey Polyakov <etrehome@gmail.com>
2012-12-06 15:41:44 +0400

    Log: added fatal error handling and uncaught exception handler

--- system/log.php
+++ system/log.php
@@ -1,7 +1,5 @@
 <?php
 
-// OK
-
 class Log
 {
 	protected static $time;
@@ -15,14 +13,18 @@ class Log
 
 		error_reporting($mode);
 		set_error_handler(array(__CLASS__,'errorHandler'),$mode);
+		set_exception_handler(array(__CLASS__,'errorHandler'));
+		register_shutdown_function(array(__CLASS__,'shutdownHandler'));
 	}
 
 	public static function disable() { self::$enabled = false; }
 	public static function enabled() { self::$enabled = true; }
 
-	public static function errorHandler($code,$message = false,$file = false,$line = false)
+	public static function errorHandler($code,$message = false,$file = false,$line = false,$backtrace = false)
 	{
-		if(!self::$enabled) return false;
+		if(!self::$enabled) return true;
+
+		$log = LOG_ERR;
 
 		if(is_object($code))
 		{
@@ -33,34 +35,50 @@ class Log
 		}
 		else
 		{
-			if (!(error_reporting() & $code)) return null;
+			if (!(error_reporting() & $code)) return true;
 
 			switch($code)
 			{
+				// Fatal errors
+				case E_COMPILE_ERROR:
+				case E_CORE_ERROR:
+				case E_PARSE:
 				case E_USER_ERROR:
-				case E_ERROR: $type = 'ERROR'; while(ob_get_level() > 1) ob_end_clean(); break;
+				case E_ERROR: $type = 'ERROR'; while(ob_get_level() > 1) ob_end_clean(); $log = LOG_ERR; break;
 
+				// Warnings
+				case E_COMPILE_WARNING:
+				case E_CORE_WARNING:
 				case E_USER_WARNING:
-				case E_WARNING: $type = 'WARNING'; break;
+				case E_RECOVERABLE_ERROR:
+				case E_WARNING: $type = 'WARNING'; $log = LOG_WARNING; break;
 
+				// Notices
+				case E_STRICT:
+				case E_USER_DEPRECATED:
+				case E_DEPRECATED:
 				case E_USER_NOTICE:
-				case E_NOTICE: $type = 'NOTICE'; break;
+				case E_NOTICE: $type = 'NOTICE'; $log = LOG_NOTICE; break;
 
-				default: $type = 'FATAL';
+				default: $type = 'UNKNOWN';
 			}
 		}
 
 		if(self::$serverRootPath) $file = str_ireplace(self::$serverRootPath,'',$file);
 
-		$message = getmypid().' ['.(round(microtime(true) - self::$time,4)).'] '. $type.' "'.$message.'"';
+		$message = getmypid().(defined('SERVER_NAME')?(' '.SERVER_NAME):'').' ['.(round(microtime(true) - self::$time,4)).'] '. $type.' "'.$message.'"';
 
-		$backtrace = array();
-
-		if(!is_object($code)) foreach(debug_backtrace() as $v) // Бэктрейс только на ошибках, так как с эксепшенов будет только до вызова этого метода внутри CMS.
+		if(!is_object($code))
 		{
-			if(!($v && isset($v['line']) && isset($v['file']))) continue;
-			if(self::$serverRootPath) $v['file'] = str_ireplace(self::$serverRootPath,'',$v['file']);
-			$backtrace[$v['file']][$v['line']] = true;
+			if(!$backtrace) $backtrace = debug_backtrace();
+
+			foreach($backtrace as $k=>$v) // Бэктрейс только на ошибках, так как с эксепшенов будет только до вызова этого метода внутри CMS.
+			{
+				unset($backtrace[$k]);
+				if(!($v && isset($v['line']) && isset($v['file']))) continue;
+				if(self::$serverRootPath) $v['file'] = str_ireplace(self::$serverRootPath,'',$v['file']);
+				$backtrace[$v['file']][$v['line']] = true;
+			}
 		}
 
 		if($backtrace)
@@ -73,15 +91,20 @@ class Log
 			$message .= ' in '.$file.':'.$line;
 		}
 
-		syslog(LOG_WARNING, $message);
+		syslog($log, $message);
 
 		return true;
+	}
+
+	public static function shutdownHandler()
+	{
+		if ($error = error_get_last() and $error['type'] & (E_ERROR|E_PARSE|E_COMPILE_ERROR|E_CORE_ERROR)) self::errorHandler($error['type'], $error['message'], $error['file'], $error['line'], debug_backtrace());
 	}
 
 	public static function debug($message)
 	{
 		if(is_array($message) || is_object($message)) $message = 'DUMP: '.var_export($message,true);
-		$message = getmypid().' ['.(round(microtime(true) - self::$time,4)).'] DEBUG: '.$message;
+		$message = getmypid().(defined('SERVER_NAME')?(' '.SERVER_NAME):'').' ['.(round(microtime(true) - self::$time,4)).'] DEBUG: '.$message;
 		syslog(LOG_INFO, $message);
 	}
 }
